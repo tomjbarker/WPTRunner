aa0adcfd9b8e105ce58f1f29d3aa858ae77ec559
Tom Barker <tomjbarker@gmail.com>
2012-05-28 03:22:16 -0400

    added wpo.r

---
+++ wpo.R
@@ -0,0 +1,44 @@
+dataDirectory <- "/Users/tbarke000/WPT/data/"
+chartDirectory <- "/Users/tbarke000/WPT/charts/"
+
+wpologs <- read.table(paste(dataDirectory, "wpo_log.csv", sep=""), header=TRUE, sep=",")
+wpochart <- paste(chartDirectory, "WPO_timeseries.pdf", sep="")
+
+
+createDataFrameByURL <- function(wpologs, url){
+df <- data.frame()
+for (i in 1:nrow(wpologs)){
+	if(wpologs$url[i] == url){
+		df <- rbind(df , wpologs[i,])
+	}
+}
+row.names(df) <- df$date
+return(df)	
+}
+
+wpologs$bytes <- wpologs$bytes / 1000 #convert bytes to KB
+
+tbdotcom <- createDataFrameByURL(wpologs, "http://tom-barker.com")
+
+
+WebSites <- c("tom-barker.com")
+WebSiteColors <- c("#ff0000", "#0000ff", "#00ff00", "#ffff00", "#ff6600", "#ff00ff", "#666666", "#ff836e", "#CCCCCC", "#a6ff6e", "#ff27a0", "#509dd9", "#00ffff", "#000000", "#b17255", "#999999")
+
+
+
+pdf(wpochart, height=12, width=12)
+par(mfrow=c(2,2))
+plot(tbdotcom$loadtime, ylim=c(1000, 40000), type="l", xaxt="n", xlab="", col="#ff0000", ylab="Load Time in Milliseconds")
+axis(1, at=1: length(row.names(tbdotcom)), lab= rownames(tbdotcom), cex.axis=0.3)
+
+
+
+plot(tbdotcom$bytes, ylim=c(500, 7462), type ="l", col="#ff0000", ylab="Page Size in KB", xlab="", xaxt="n")
+axis(1, at=1: length(row.names(tbdotcom)), lab= rownames(tbdotcom), cex.axis=0.3)
+
+plot(tbdotcom$httprequests, ylim=c(10, 300), type ="l", col="#ff0000", ylab="HTTP Requests", xlab="", xaxt="n")	
+axis(1, at=1: length(row.names(tbdotcom)), lab= rownames(tbdotcom), cex.axis=0.3)
+
+plot(tbdotcom$httprequests, type ="n", xlab="", ylab="", xaxt="n", yaxt="n", frame=FALSE)	
+legend("topright", inset=.05, title="Legend", WebSites, lty=c(1,2,1,1,1,1,1,1,1,1,1,1,1,1,1,2), col= WebSiteColors)
+dev.off()
